921e8f5b259276a02f1dae37f269200b129b1f68
DableBlock <1255887634@qq.com>
2018-05-29 23:06:20 +0800

    带有外部状态的生成器函数

---
+++ Chapter4/4.6.py
@@ -0,0 +1,24 @@
+# 带有外部状态的生成器函数
+# 定义一个生成器函数，但是它会调用某个你想暴露给用户使用的外部状态值
+from collections import deque
+
+
+class linehistory:
+    def __init__(self, lines, histlen=3):
+        self.lines = lines
+        self.history = deque(maxlen=histlen)
+
+    def __iter__(self):
+        for lineno, line in enumerate(self.lines, 1):
+            self.history.append((lineno, line))
+            yield line
+
+    def clear(self):
+        self.history.clear()
+
+with open('somefile.txt') as f:
+    lines = linehistory(f)
+    for line in lines:
+        if 'python' in line:
+            for lineno, hline in lines.history:
+                print('{}:{}'.format(lineno, hline), end='')
